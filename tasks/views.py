d67d8971d7a6d7c827e58dff8ce52d61f1575e44
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-12-01 13:59:33 -0600

    Creación de usuarios

--- tasks/views.py
+++ tasks/views.py
@@ -1,13 +1,35 @@
+#recibe respuestas http
 from django.http import HttpResponse
+# Permite renderizar los templates html 
 from django.shortcuts import render
 
 # django por defecto ya contiene un formularario para la autenticaion de usarios y para utilizarlo debemos de importar la siguiente clase ->UserCreationForm
 from django.contrib.auth.forms import UserCreationForm
+# Para crear usuarios importa la clase User
+from django.contrib.auth.models import User
+
 
 # Create your views here.
 
-def helloword(request):
-    title = 'Hello world'
-    return render(request, 'singup.html',{
-        'form' : UserCreationForm #-> crea un formulario creado en usercreationform y se renderiza a singup.html
+
+#view to create the form and create users
+def home(request):
+    return render(request, 'home.html')
+def singup(request):
+    if request.method == 'GET':
+        print('Enviando formulario')
+        return render(request, 'singup.html',{
+        'form': UserCreationForm
     })
+    else:
+        if request.POST['password1'] == request.POST['password2']:
+            try:
+                #register user 
+                user = User.objects.create_user(username=request.POST['username'], password=request.POST['password1'])
+                user.save()
+                return HttpResponse('User created successfuly')
+            except:
+                return HttpResponse('User already exists')
+        return HttpResponse('Password do not match')
+        
+    
