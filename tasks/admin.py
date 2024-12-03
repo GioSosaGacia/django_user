ef34d1de3da240444077e390678707463684842b
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-12-02 21:11:39 -0600

    Task model, createsuperuser, add task inside the UI, and register the models created

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Task
 
+# esta clase nos permite ver en la interfaz de admin campos de solo lectura como el campo created ya que es un campo que se llena de manera automatica al crear cualquier tarea
+# Hereda todo lo que este en admin.ModelAdmin
+class TaskAdmin(admin.ModelAdmin):
+    # Indicamos que son campos de solo lectura, colocamos una coma al final porque es una tupla
+    readonly_fields = ("created", )
+    
 # Register your models here.
+admin.site.register(Task, TaskAdmin)
--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+# iMPORTAMOS LA clase User para hacer la relacion de la tabla Task con User
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    datecompleted = models.DateTimeField(null=True)
+    important = models.BooleanField(default=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #elimina datos en cascada
+    
+    # esta funcion nos permite ver el titulo de la tarea que de agrego en la interfaz, antes de esta funcion solo mostraba task1
+    def __str__(self):
+        return self.title + ' -by ' + self.user.username
