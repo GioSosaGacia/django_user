ea6bc99dc3a222fb7c48fb3c2c8e29e66d0d4eb1
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-12-08 17:15:52 -0600

    comente la linea de requirement.txt dentro de build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # Modify this line as needed for your package manager (pip, poetry, etc.)
-pip install -r requirements.txt
+#pip install -r requirements.txt lo comentamos porque ya lo esta ejecutando render
 
 # Convert static asset files
 python manage.py collectstatic --no-input
