019b1fcd7d38ed221bfbf06942774e4f41ad2ecf
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-12-08 09:53:44 -0600

    Agregando estilos bootstrap

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,10 +1,19 @@
-from django.forms import ModelForm
+#from django.forms import ModelForm
+#importamos esta clase para poder acceder a form 
+from django import forms
 # Importamos el modelo con el cual se creara el formulario
 from .models import Task
 
-class TaskForm(ModelForm):
-    class Meta():
+# class TaskForm(ModelForm):
+class TaskForm(forms.ModelForm):
+    class Meta:
         # indicamos que modelo es el que tomará como referencia para crear el formularios
         model = Task
         # creamos una lista con los campos que deseamos agregar formulario
         fields = ['title','description','important']
+        widgets = {
+            # class es atributo de html : from-control es la clase de bootstrap que quiero aplicar
+            'title': forms.TextInput(attrs={'class':'form-control', 'placeholder':'Write a title'}),
+            'description': forms.Textarea(attrs={'class':'form-control','placeholder':'Write a description'}),
+            'important': forms.CheckboxInput(attrs={'class':'form-check-input'})
+        }
