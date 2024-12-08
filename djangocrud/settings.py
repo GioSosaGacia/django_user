a0cea2b1c9f7674c12f08a7f39e622c9bd3c2149
GioSosaGacia <giovanni-sosa-12@outlook.com>
2024-12-08 10:57:47 -0600

    Agregando imagen a home, archivos settings, carpeta static + imagen dentr, uso de load static en la parte superior del html and static dentro de la ancla a

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -116,7 +116,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 #esta relacionada a login-required, la clase importada en views.py
 LOGIN_URL = '/singin'
